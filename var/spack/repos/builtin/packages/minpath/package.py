35df3de2b05c26a261e8d8b84d91a26b424aa846
Luke Diorio-Toth <ldioriototh@gmail.com>
2023-02-13 11:53:46 -0600

    changed minpath to github
    the old versions (1.2,1.4) are python 2 only

--- var/spack/repos/builtin/packages/minpath/package.py
+++ var/spack/repos/builtin/packages/minpath/package.py
@@ -3,22 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-minpath
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-minpath
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 
 from spack.package import *
 
@@ -30,13 +14,14 @@ class Minpath(Package):
     query dataset"""
 
     homepage = "https://omics.informatics.indiana.edu/MinPath/"
-    url = "https://omics.informatics.indiana.edu/mg/get.php?justdoit=yes&software=minpath1.4.tar.gz"
+    git = "https://github.com/mgtools/MinPath.git"
 
-    version("1.4", sha256="665e90b5ee7fa5837d13b1145cdf3eafa691d25c1ce4bb76847dfa771ff24551")
-    version("1.2", sha256="f57a7c6a83d5ae366e08069d4b12a7c12fae8aa25d35fea5cfbe75cacbcd04de")
+    # versions 1.5+ are hosted on GitHub
+    version("1.6", commit="46d3e81a4dca2310d558bea970bc002b15d44767")
+    version("1.5", commit="8745150d61b4670876e2728800ca549fdd9e0747")
 
     depends_on("python")
 
-    def install(self, spec, prefix):
-        mkdirp(prefix.bin)
-        install("MinPath{0}.py".format(self.version), prefix.bin)
+#    def install(self, spec, prefix):
+#        mkdirp(prefix.bin)
+#        install("MinPath{0}.py".format(self.version), prefix.bin)
